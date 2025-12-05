c2407faab4d3840bb4681bcaf3b20dfca9013cd2
unknown <punithasekar333@gmail.com>
2025-12-05 11:45:30 +0530

    added method

--- src/main/java/com/jspider/Main.java
+++ src/main/java/com/jspider/Main.java
@@ -6,6 +6,9 @@ public class Main {
     public static void main(String[] args) {
         
         String pass="jesuschrist@123";
+
     }
-    public sta
+    public static void adduser(){
+
+    }
 }
